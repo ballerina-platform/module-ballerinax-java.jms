ed6182483b705a951e6f1d262130441da1e1ede3
Indika Sampath <indikasampath2000@gmail.com>
2019-10-01 17:41:51 +0530

    Update the VERSION constant to 0.6.1

--- utils/src/main/java/org/wso2/ei/b7a/jms/Constants.java
+++ utils/src/main/java/org/wso2/ei/b7a/jms/Constants.java
@@ -35,7 +35,7 @@ public class Constants {
 
     static final String ORG = "org";
     static final String PACKAGE_NAME = "wso2/jms";
-    public static final String VERSION = "0.6.0";
+    public static final String VERSION = "0.6.1";
 
     public static final String PACKAGE_NAME_WITH_VERSION = PACKAGE_NAME + VERSION_SEPARATOR + VERSION;
 
