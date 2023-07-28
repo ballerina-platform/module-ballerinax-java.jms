5a26b4afabf8f4560c18b0e5c4b860ceea7628f0
ayeshLK <ayesh9303@gmail.com>
2023-07-28 18:07:04 +0530

    Remove unused imports

--- native/src/main/java/io/ballerina/stdlib/java.jms/JmsConnection.java
+++ native/src/main/java/io/ballerina/stdlib/java.jms/JmsConnection.java
@@ -24,8 +24,6 @@ import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.api.values.BString;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Iterator;
