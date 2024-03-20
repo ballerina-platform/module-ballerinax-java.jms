f4b9352c2bfdaece5ebae5cd24f536da1d0a9a4e
ayeshLK <ayesh9303@gmail.com>
2024-03-20 12:06:51 +0530

    Add null-checks for the nullable fields in the JMS message

--- native/src/main/java/io/ballerina/stdlib/java.jms/CommonUtils.java
+++ native/src/main/java/io/ballerina/stdlib/java.jms/CommonUtils.java
@@ -106,7 +106,9 @@ public class CommonUtils {
         BMap<BString, Object> ballerinaMessage = ValueCreator.createRecordValue(ModuleUtils.getModule(), messageType);
         ballerinaMessage.put(Constants.MESSAGE_ID, StringUtils.fromString(message.getJMSMessageID()));
         ballerinaMessage.put(Constants.TIMESTAMP, message.getJMSTimestamp());
-        ballerinaMessage.put(Constants.CORRELATION_ID, StringUtils.fromString(message.getJMSCorrelationID()));
+        if (Objects.nonNull(message.getJMSCorrelationID())) {
+            ballerinaMessage.put(Constants.CORRELATION_ID, StringUtils.fromString(message.getJMSCorrelationID()));
+        }
         if (Objects.nonNull(message.getJMSReplyTo())) {
             ballerinaMessage.put(Constants.REPLY_TO, getJmsDestinationField(message.getJMSReplyTo()));
         }
@@ -115,7 +117,9 @@ public class CommonUtils {
         }
         ballerinaMessage.put(Constants.DELIVERY_MODE, message.getJMSDeliveryMode());
         ballerinaMessage.put(Constants.REDELIVERED, message.getJMSRedelivered());
-        ballerinaMessage.put(Constants.JMS_TYPE, StringUtils.fromString(message.getJMSType()));
+        if (Objects.nonNull(message.getJMSType())) {
+            ballerinaMessage.put(Constants.JMS_TYPE, StringUtils.fromString(message.getJMSType()));
+        }
         ballerinaMessage.put(Constants.EXPIRATION, message.getJMSExpiration());
         try {
             ballerinaMessage.put(Constants.DELIVERED_TIME, message.getJMSDeliveryTime());
